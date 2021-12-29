61a653098c92bb15739313ecba6039f1afd18e90
Femi Novia Lina <feminovi@gmail.com>
2021-12-29 20:34:53 +0700

    initial commit

---
+++ src/main/java/com/fnvls/finalprojectbankingcrowdacademy/FinalProjectBankingCrowdAcademyApplication.java
@@ -0,0 +1,13 @@
+package com.fnvls.finalprojectbankingcrowdacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectBankingCrowdAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectBankingCrowdAcademyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fnvls/finalprojectbankingcrowdacademy/FinalProjectBankingCrowdAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fnvls.finalprojectbankingcrowdacademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectBankingCrowdAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
